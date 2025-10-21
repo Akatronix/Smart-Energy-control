5ae958c5b876fbb675401d92734deaa181049348
Akatronix <xazan6501@gmail.com>
2025-10-21 08:08:42 +0100

    updated the hospot

--- client/src/utils/HospotRequest.js
+++ client/src/utils/HospotRequest.js
@@ -7,7 +7,7 @@ const handleConfirm = async ({ ssid, password }) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        id: "68f6e4c40d3af76a142703c0",
+        id: import.meta.env.VITE_HOTSPOT_ID,
         ssid,
         password,
       }),
